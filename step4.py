0073b920d810f76902104179f86d8d8cde9a6c03
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2026-08-15 11:47:51 +0600

    Update step4.py

--- step4.py
+++ step4.py
@@ -2,12 +2,13 @@
 
 import pandas as pd
 
-# 1. Load the files
+# Load the files
 df = pd.read_csv(r'C:\Users\THINKPAD\Downloads\frequency_cleaned.csv', encoding='utf-8')
 dv = pd.read_csv(r'C:\Users\THINKPAD\Downloads\velocity_cleaned.csv', encoding='utf-8')
 
 df_organized = df.sort_values(by=['Node', 'TimeSlot', 'VehicleType', 'Vehicle'])
 df_organized = df_organized[['Node', 'TimeSlot', 'VehicleType', 'Vehicle','Total_PCU']]
 
+# save the outfut file in your pc
 df_organized.to_csv(r'C:\Users\THINKPAD\Downloads\frequency_cleaned_organized.csv', index=False, encoding='utf-8')
 
